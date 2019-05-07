fe5dcc7412748d10f94c7cad5dd8a1185e80f9d2
Joseph Hyatt <josephnhyatt@gmail.com>
2019-05-06 17:20:18 -0700

    adding learn_co labs

---
+++ flatiron-store-project-v-000/app/controllers/categories_controller.rb
@@ -0,0 +1,12 @@
+class CategoriesController < ApplicationController
+  before_action :set_category, only: :show
+
+  def index
+    @categories = Category.all
+  end
+
+  private
+  def set_category
+    @category = Category.find_by(id: params[:id])
+  end
+end
---
+++ flatiron-store-project-v-000/db/seeds.rb
@@ -0,0 +1,19 @@
+5.times do |i|
+  User.create(email: "test#{i}@email.com", password: 'password')
+end
+
+10.times do
+  Item.create(
+    title: Faker::Commerce.product_name,
+    inventory: Faker::Number.number(2),
+    price: Faker::Number.number(4)
+  )
+  Category.create(title: Faker::Commerce.department)
+end
+
+counter = 1
+Item.all.each do |item|
+  item.category_id = counter
+  item.save
+  counter += 1
+end
