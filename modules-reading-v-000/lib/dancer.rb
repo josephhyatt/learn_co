a158150c3a72f499a3587eedac853a4577f37f06
Joseph Hyatt <josephnhyatt@gmail.com>
2019-05-06 17:48:38 -0700

    adding learn_co labs

---
+++ modules-reading-v-000/lib/dancer.rb
@@ -0,0 +1,16 @@
+#includes the Dance module
+#has a name
+#extends the MetaDancing module
+
+require_relative './fancy_dance.rb'
+
+class Dancer
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+end
---
+++ my-first-hash-v-000/my_first_hash.rb
@@ -0,0 +1,32 @@
+  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
+def my_hash
+  my_hash = {
+    "name" => "Joseph Hyatt",
+    "age" => 31,
+    "pimp" => true
+  }
+end
+
+#set a variable called shipping_manifest equal to a hash.
+def shipping_manifest
+  shipping_manifest = {
+    "whale bone corsets" => 5,
+    "porcelain vases" => 2,
+    "oil paintings" => 3
+  }
+end
+
+#Use the [] hash method to return the value of the "oil paintings" key of the shipping_manifest hash.
+def retrieval
+  shipping_manifest = { "whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3 }
+
+  shipping_manifest["oil paintings"]
+end
+
+#Use the []= method to add the following key/value pair to the hash: "pearl necklace" => 1 and return the newly-added-to shipping_manifest.
+def adding
+  shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
+
+  shipping_manifest["pearl necklace"] = 1
+  return shipping_manifest
+end
